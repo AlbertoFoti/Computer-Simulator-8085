c2f1d146742c84e6b6643bae687db26dc2cb59e3
Alberto <albertofots@gmail.com>
2022-09-19 12:20:57 +0200

    feat: BUS data load. CPU-BUS interaction

--- ComputerSimulator8085/Core/Bus/Bus.cpp
+++ ComputerSimulator8085/Core/Bus/Bus.cpp
@@ -37,29 +37,31 @@ void Bus::load(uint16_t CONTENT) {
     this->multiplexedWord = (uint8_t)CONTENT;
     this->addressBusHigh = (uint8_t)(CONTENT >> 8);
 
-    /*
+    if(std::shared_ptr<CPU> cpu_shared = this->cpu.lock()) {
+        /*
         S0 = 1, S1 = 0  -> Memory Write Machine Cycle (MWMC)
                -> WRbar = 1  :  latch address into memory buffers
                -> WRbar = 0  :  write data to memory (data to address latched in the memory module)
         S0 = 0, S1 = 1  -> Memory Read Machine Cycle  (MRMC)
                -> latch address, then get() data from memory
-    */
-    /*
-    if (cpu->CTRL_SIG.S0 == 1 && cpu->CTRL_SIG.S1 == 0) {         // MWMC   Write
-        if (cpu->CTRL_SIG.WRbar == 1) {
+        */
+        if (cpu_shared->CTRL_SIG.S0 == 1 && cpu_shared->CTRL_SIG.S1 == 0) {         // MWMC   Write
+            if (cpu_shared->CTRL_SIG.WRbar == 1) {
+                this->ram->latchAddress(CONTENT);
+            }
+            else if (cpu_shared->CTRL_SIG.WRbar == 0) {
+                this->ram->set((uint8_t)CONTENT);
+            }
+        }
+        else if (cpu_shared->CTRL_SIG.S0 == 0 && cpu_shared->CTRL_SIG.S1 == 1) {    // MRMC   Read
             this->ram->latchAddress(CONTENT);
+            uint8_t res_byte = this->ram->get(CONTENT);
+            this->addressBusHigh = 0x00;
+            this->multiplexedWord = res_byte;
         }
-        else if (cpu->CTRL_SIG.WRbar == 0) {
-            this->ram->set((uint8_t)CONTENT);
-        }
+    } else {
+        std::cout << "CPU not connected to BUS !" << std::endl;
     }
-    else if (cpu->CTRL_SIG.S0 == 0 && cpu->CTRL_SIG.S1 == 1) {    // MRMC   Read
-        this->ram->latchAddress(CONTENT);
-        uint8_t res_byte = this->ram->get(CONTENT);
-        this->addressBusHigh = 0x00;
-        this->multiplexedWord = res_byte;
-    }
-     */
 }
 
 /*
